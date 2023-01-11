4a5f824fe6fd80e73d5182827d24bf2477af6ef4
bytemaniak <bytemaniak98@gmail.com>
2023-01-11 22:34:14 +0000

    Initialize project.

---
+++ src/main/java/com/bytemaniak/mcuake/MCuake.java
@@ -0,0 +1,13 @@
+package net.fabricmc.example;
+
+import net.fabricmc.api.ModInitializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class MCuake implements ModInitializer {
+	public static final Logger LOGGER = LoggerFactory.getLogger("modid");
+
+	@Override
+	public void onInitialize() {
+	}
+}
---
+++ src/main/java/com/bytemaniak/mcuake/mixin/MCuakePlayer.java
@@ -0,0 +1,8 @@
+package com.bytemaniak.mcuake.mixin;
+
+import org.spongepowered.asm.mixin.Mixin;
+import net.minecraft.entity.PlayerEntity;
+
+@Mixin(PlayerEntity.class)
+public class MCuakePlayer {
+}

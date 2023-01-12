1b9f0c49069c97ef1c4a4db17ba8ff7bc5386766
bytemaniak <bytemaniak98@gmail.com>
2023-01-12 22:39:12 +0000

    Added multi tool item. No functionality yet.

--- src/main/java/com/bytemaniak/mcuake/MCuake.java
+++ src/main/java/com/bytemaniak/mcuake/MCuake.java
@@ -1,6 +1,7 @@
 package com.bytemaniak.mcuake;
 
 import com.bytemaniak.mcuake.blocks.MCuakeJumppad;
+import com.bytemaniak.mcuake.items.MCuakeTool;
 import com.bytemaniak.mcuake.registry.MusicDiscRegistry;
 import net.fabricmc.api.ModInitializer;
 import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
@@ -20,11 +21,11 @@ public class MCuake implements ModInitializer {
 
 	public static final Block JUMPPAD_BLOCK = new MCuakeJumppad();
 
-	public static final Identifier THRU_THE_MIRROR_IDENT = new Identifier("mcuake", "thru_the_mirror");
 	@Override
 	public void onInitialize() {
 		Registry.register(Registries.BLOCK, new Identifier("mcuake", "jumppad"), JUMPPAD_BLOCK);
 		Registry.register(Registries.ITEM, new Identifier("mcuake", "jumppad"), new BlockItem(JUMPPAD_BLOCK, new Item.Settings()));
+		Registry.register(Registries.ITEM, new Identifier("mcuake", "tool"), new MCuakeTool());
 
 		MusicDiscRegistry.LoadDiscs();
 	}
---
+++ src/main/java/com/bytemaniak/mcuake/items/MCuakeTool.java
@@ -0,0 +1,10 @@
+package com.bytemaniak.mcuake.items;
+
+import net.minecraft.block.Material;
+import net.minecraft.item.Item;
+
+public class MCuakeTool extends Item{
+    public MCuakeTool() {
+        super(new Item.Settings().maxCount(1));
+    }
+}

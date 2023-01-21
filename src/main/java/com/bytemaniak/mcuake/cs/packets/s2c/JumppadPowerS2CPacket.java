d471cc36f548ccb3113cb0998722b86e5ef58612
bytemaniak <bytemaniak98@gmail.com>
2023-01-21 16:28:58 +0000

    Push files omitted in last commit

---
+++ src/main/java/com/bytemaniak/mcuake/cs/packets/s2c/JumppadPowerS2CPacket.java
@@ -0,0 +1,21 @@
+package com.bytemaniak.mcuake.cs.packets.s2c;
+
+import com.bytemaniak.mcuake.blocks.jumppad.JumppadEntity;
+import net.fabricmc.fabric.api.networking.v1.PacketSender;
+import net.minecraft.block.entity.BlockEntity;
+import net.minecraft.client.MinecraftClient;
+import net.minecraft.client.network.ClientPlayNetworkHandler;
+import net.minecraft.network.PacketByteBuf;
+import net.minecraft.util.math.BlockPos;
+
+public class JumppadPowerS2CPacket {
+    public static void receive(MinecraftClient client, ClientPlayNetworkHandler handler, PacketByteBuf buf, PacketSender sender)
+    {
+        BlockPos pos = buf.readBlockPos();
+        BlockEntity entity = MinecraftClient.getInstance().world.getBlockEntity(pos);
+        if (entity instanceof JumppadEntity)
+        {
+            ((JumppadEntity) entity).updatePower(buf.readFloat(), buf.readFloat());
+        }
+    }
+}

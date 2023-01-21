875036f56c97dfa942ba4cbd2a4e090c15223610
bytemaniak <bytemaniak98@gmail.com>
2023-01-21 16:35:13 +0000

    Remove attributes code as it is not useful for setting player stats

--- src/main/java/com/bytemaniak/mcuake/MCuake.java
+++ src/main/java/com/bytemaniak/mcuake/MCuake.java
@@ -15,7 +15,6 @@ import net.fabricmc.api.ModInitializer;
 import net.fabricmc.fabric.api.itemgroup.v1.FabricItemGroup;
 import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
 import net.fabricmc.fabric.api.object.builder.v1.block.entity.FabricBlockEntityTypeBuilder;
-import net.fabricmc.fabric.api.object.builder.v1.entity.FabricDefaultAttributeRegistry;
 import net.fabricmc.fabric.api.object.builder.v1.entity.FabricEntityTypeBuilder;
 import net.fabricmc.fabric.api.screenhandler.v1.ExtendedScreenHandlerType;
 import net.minecraft.block.Block;
@@ -23,10 +22,6 @@ import net.minecraft.block.entity.BlockEntityType;
 import net.minecraft.entity.EntityDimensions;
 import net.minecraft.entity.EntityType;
 import net.minecraft.entity.SpawnGroup;
-import net.minecraft.entity.attribute.ClampedEntityAttribute;
-import net.minecraft.entity.attribute.DefaultAttributeContainer;
-import net.minecraft.entity.attribute.EntityAttribute;
-import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.BlockItem;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemGroup;
@@ -65,16 +60,8 @@ public class MCuake implements ModInitializer {
 			.icon(() -> new ItemStack(PLAYER_SETTINGS))
 			.build();
 
-	public static final EntityAttribute MCUAKE_HEALTH = new ClampedEntityAttribute("attributes.mcuake.player_health", 100, 0, 200);
-	public static final EntityAttribute MCUAKE_ARMOR = new ClampedEntityAttribute("attributes.mcuake.player_armor", 100, 0, 200);
-
 	static {
 		Registry.register(Registries.SCREEN_HANDLER, new Identifier("mcuake", "jumppad_power"), JUMPPAD_SCREEN_HANDLER);
-	}
-
-	private DefaultAttributeContainer.Builder createPlayerAttributes()
-	{
-		return PlayerEntity.createPlayerAttributes().add(MCuake.MCUAKE_HEALTH, 100).add(MCuake.MCUAKE_ARMOR, 0);
 	}
 
 	@Override
@@ -89,10 +76,6 @@ public class MCuake implements ModInitializer {
 
 		Sounds.loadSounds();
 		MusicDiscRegistry.LoadDiscs();
-
-		Registry.register(Registries.ATTRIBUTE, new Identifier("mcuake", "player_health"), MCUAKE_HEALTH);
-		Registry.register(Registries.ATTRIBUTE, new Identifier("mcuake", "player_armor"), MCUAKE_ARMOR);
-		FabricDefaultAttributeRegistry.register(EntityType.PLAYER, createPlayerAttributes());
 
 		CSMessages.registerServerPackets();
 	}
--- src/main/java/com/bytemaniak/mcuake/entity/projectile/PlasmaBall.java
+++ src/main/java/com/bytemaniak/mcuake/entity/projectile/PlasmaBall.java
@@ -4,15 +4,12 @@ import com.bytemaniak.mcuake.MCuake;
 import com.bytemaniak.mcuake.registry.DamageSources;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.EntityType;
-import net.minecraft.entity.attribute.EntityAttributeInstance;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.entity.projectile.*;
 import net.minecraft.sound.SoundEvents;
 import net.minecraft.util.hit.EntityHitResult;
 import net.minecraft.util.hit.HitResult;
 import net.minecraft.world.World;
-
-import javax.management.Attribute;
 
 public class PlasmaBall extends ExplosiveProjectileEntity {
     private float PLASMABALL_DAMAGE = 10;
@@ -29,22 +26,16 @@ public class PlasmaBall extends ExplosiveProjectileEntity {
     @Override
     protected void onEntityHit(EntityHitResult entityHitResult) {
         super.onEntityHit(entityHitResult);
-        Entity entity = entityHitResult.getEntity();
-        if (entity instanceof PlayerEntity playerEntity && playerEntity.isAlive()) {
-            this.getOwner().playSound(SoundEvents.ENTITY_EXPERIENCE_ORB_PICKUP, 1, .5f);
-            EntityAttributeInstance playerHealthAttribute = playerEntity.getAttributeInstance(MCuake.MCUAKE_HEALTH);
-            double playerHealth = playerHealthAttribute.getValue() - PLASMABALL_DAMAGE;
-            playerHealthAttribute.setBaseValue(playerHealth);
-            MCuake.LOGGER.info(String.valueOf(playerHealth));
-            if (playerHealth <= 0)
-            {
-                entity.damage(DamageSources.PLASMAGUN_DAMAGE, Integer.MAX_VALUE);
-                playerHealthAttribute.setBaseValue(100);
+
+        if (!this.world.isClient) {
+            Entity entity = entityHitResult.getEntity();
+            if (entity instanceof PlayerEntity playerEntity && playerEntity.isAlive()) {
+                this.getOwner().playSound(SoundEvents.ENTITY_EXPERIENCE_ORB_PICKUP, 1, .5f);
+            } else {
+                entity.damage(DamageSources.PLASMAGUN_DAMAGE, PLASMABALL_DAMAGE / 4);
             }
-        }
-        else
-        {
-            entity.damage(DamageSources.PLASMAGUN_DAMAGE, PLASMABALL_DAMAGE / 4);
+
+            this.kill();
         }
     }
 

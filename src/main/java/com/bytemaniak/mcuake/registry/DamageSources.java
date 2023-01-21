810e5303ce7f8ab755f40296ee665f82de078dd3
bytemaniak <bytemaniak98@gmail.com>
2023-01-21 14:50:50 +0000

    Add damage sources for all Quake3 weapons and dummy langs for them.

--- src/main/java/com/bytemaniak/mcuake/registry/DamageSources.java
+++ src/main/java/com/bytemaniak/mcuake/registry/DamageSources.java
@@ -3,6 +3,15 @@ package com.bytemaniak.mcuake.registry;
 import net.minecraft.entity.damage.DamageSource;
 
 public class DamageSources {
+    public static final DamageSource GAUNTLET_DAMAGE = new DamageSource("mcuake.gauntlet");
+    public static final DamageSource MACHINEGUN_DAMAGE = new DamageSource("mcuake.machinegun");
+    public static final DamageSource SHOTGUN_DAMAGE = new DamageSource("mcuake.shotgun");
+    public static final DamageSource GRENADE_DAMAGE = new DamageSource("mcuake.grenade");
+    public static final DamageSource ROCKET_DAMAGE = new DamageSource("mcuake.rocket");
+    public static final DamageSource LIGHTNING_DAMAGE = new DamageSource("mcuake.lightning");
+    public static final DamageSource RAILGUN_DAMAGE = new DamageSource("mcuake.railgun");
     public static final DamageSource PLASMAGUN_DAMAGE = new DamageSource("mcuake.plasmagun");
+    public static final DamageSource BFG10K_DAMAGE = new DamageSource("mcuake.bfg10k");
+
     public static final DamageSource SPIKES = new DamageSource("mcuake.spikes");
 }

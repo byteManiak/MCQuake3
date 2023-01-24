af08dc4885ce7b757ddd58ba6040b08bd49c5ca9
bytemaniak <bytemaniak98@gmail.com>
2023-01-24 17:19:23 +0000

    Play the ding sound only to the player that shot the plasmagun

--- src/main/java/com/bytemaniak/mcuake/entity/projectile/PlasmaBall.java
+++ src/main/java/com/bytemaniak/mcuake/entity/projectile/PlasmaBall.java
@@ -2,6 +2,10 @@ package com.bytemaniak.mcuake.entity.projectile;
 
 import com.bytemaniak.mcuake.MCuake;
 import com.bytemaniak.mcuake.registry.DamageSources;
+import net.fabricmc.api.EnvType;
+import net.fabricmc.api.Environment;
+import net.minecraft.client.MinecraftClient;
+import net.minecraft.client.sound.PositionedSoundInstance;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.EntityType;
 import net.minecraft.entity.player.PlayerEntity;
@@ -18,21 +22,35 @@ public class PlasmaBall extends ExplosiveProjectileEntity {
 
     public PlasmaBall(World world) { super(MCuake.PLASMA_BALL, world); }
 
+    @Environment(EnvType.SERVER)
+    private void doDamage(Entity entity)
+    {
+        if (entity instanceof PlayerEntity playerEntity && playerEntity.isAlive()) {
+        } else {
+            entity.damage(DamageSources.PLASMAGUN_DAMAGE, PLASMABALL_DAMAGE / 4);
+        }
+
+        this.kill();
+    }
+
+    @Environment(EnvType.CLIENT)
+    private void playDingLocal(Entity entity)
+    {
+        if (this.getOwner().getUuid() == MinecraftClient.getInstance().player.getUuid() &&
+                entity instanceof PlayerEntity playerEntity &&
+                playerEntity.isAlive()) {
+            MinecraftClient.getInstance().getSoundManager().play(PositionedSoundInstance.master(SoundEvents.ENTITY_EXPERIENCE_ORB_PICKUP, 1.f, .75f));
+        }
+    }
+
     @Override
     protected void onEntityHit(EntityHitResult entityHitResult) {
         super.onEntityHit(entityHitResult);
-
+        Entity entity = entityHitResult.getEntity();
         if (!this.world.isClient) {
-            Entity entity = entityHitResult.getEntity();
-            if (entity instanceof PlayerEntity playerEntity && playerEntity.isAlive()) {
-                this.getOwner().playSound(SoundEvents.ENTITY_EXPERIENCE_ORB_PICKUP, 1, .5f);
-            } else {
-                entity.damage(DamageSources.PLASMAGUN_DAMAGE, PLASMABALL_DAMAGE / 4);
-            }
-
-            this.kill();
+            doDamage(entity);
         } else {
-
+            playDingLocal(entity);
         }
     }
 

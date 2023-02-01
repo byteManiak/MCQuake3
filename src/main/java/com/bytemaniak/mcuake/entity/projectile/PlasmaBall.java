c6d0c6b3bc906ffebe61842137c8ceccc26c9a6e
bytemaniak <bytemaniak98@gmail.com>
2023-02-01 18:04:17 +0000

    Greatly reduce lifetime of plasma ball

--- src/main/java/com/bytemaniak/mcuake/entity/projectile/PlasmaBall.java
+++ src/main/java/com/bytemaniak/mcuake/entity/projectile/PlasmaBall.java
@@ -8,7 +8,7 @@ public class PlasmaBall extends SimpleProjectile {
     private static int PLASMAGUN_DAMAGE = 10;
 
     public PlasmaBall(EntityType<? extends SimpleProjectile> entityType, World world) {
-        super(entityType, world, PLASMAGUN_DAMAGE, "machinegun", 400);
+        super(entityType, world, PLASMAGUN_DAMAGE, "machinegun", 75);
     }
 
     public PlasmaBall(World world) { this(MCuake.PLASMA_BALL, world); }

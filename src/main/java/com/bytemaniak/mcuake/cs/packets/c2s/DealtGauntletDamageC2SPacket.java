337478ffa9fa06176699ef2808a1e4247b2ca857
bytemaniak <bytemaniak98@gmail.com>
2023-02-10 18:31:28 +0000

    Add Quake damage for Gauntlet.

--- src/main/java/com/bytemaniak/mcuake/cs/packets/c2s/DealtGauntletDamageC2SPacket.java
+++ src/main/java/com/bytemaniak/mcuake/cs/packets/c2s/DealtGauntletDamageC2SPacket.java
@@ -1,5 +1,6 @@
 package com.bytemaniak.mcuake.cs.packets.c2s;
 
+import com.bytemaniak.mcuake.entity.MCuakePlayer;
 import com.bytemaniak.mcuake.registry.DamageSources;
 import net.fabricmc.fabric.api.networking.v1.PacketSender;
 import net.minecraft.entity.Entity;
@@ -9,8 +10,15 @@ import net.minecraft.server.network.ServerPlayNetworkHandler;
 import net.minecraft.server.network.ServerPlayerEntity;
 
 public class DealtGauntletDamageC2SPacket {
+    private static final int GAUNTLET_QUAKE_DAMAGE = 5;
+    private static final int GAUNTLET_MC_DAMAGE = 2;
+
     public static void receive(MinecraftServer server, ServerPlayerEntity player, ServerPlayNetworkHandler handler, PacketByteBuf buf, PacketSender sender) {
         Entity attacked = player.world.getEntityById(buf.readInt());
-        attacked.damage(DamageSources.GAUNTLET_DAMAGE, 5);
+        if (attacked instanceof MCuakePlayer quakePlayer) {
+            quakePlayer.takeDamage(GAUNTLET_QUAKE_DAMAGE, DamageSources.GAUNTLET_DAMAGE);
+        } else {
+            attacked.damage(DamageSources.GAUNTLET_DAMAGE, GAUNTLET_MC_DAMAGE);
+        }
     }
 }

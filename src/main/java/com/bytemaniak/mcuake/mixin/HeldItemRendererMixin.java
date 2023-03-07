ed3a3dff1a962ce4ea900b1478eeb8e952f4cad5
bytemaniak <bytemaniak98@gmail.com>
2023-03-07 17:44:37 +0000

    Don't play bow animation for weapons in first person

---
+++ src/main/java/com/bytemaniak/mcuake/mixin/HeldItemRendererMixin.java
@@ -0,0 +1,20 @@
+package com.bytemaniak.mcuake.mixin;
+
+import com.bytemaniak.mcuake.items.Weapon;
+import net.minecraft.client.render.item.HeldItemRenderer;
+import net.minecraft.item.ItemStack;
+import net.minecraft.util.UseAction;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Redirect;
+
+@Mixin(HeldItemRenderer.class)
+public class HeldItemRendererMixin {
+    @Redirect(method = "renderFirstPersonItem", at = @At(value = "INVOKE", target = "Lnet/minecraft/item/ItemStack;getUseAction()Lnet/minecraft/util/UseAction;"))
+    private UseAction cancelFirstPersonBowAnimation(ItemStack instance) {
+        if (instance.getItem() instanceof Weapon) {
+            return UseAction.NONE;
+        }
+        return instance.getUseAction();
+    }
+}

e97ef7fd2d55d133915e204c6aae40cd10480fe0
bytemaniak <bytemaniak98@gmail.com>
2023-07-25 16:28:51 +0100

    Always play the weapon change sound when switching to a Quake weapon

--- src/main/java/com/bytemaniak/mcuake/mixin/PlayerMixin.java
+++ src/main/java/com/bytemaniak/mcuake/mixin/PlayerMixin.java
@@ -77,8 +77,8 @@ public abstract class PlayerMixin extends LivingEntity implements QuakePlayer {
         ItemStack currentMainHandStack = getEquippedStack(EquipmentSlot.MAINHAND);
         if (!world.isClient && currentMainHandStack.getItem() instanceof Weapon && currentMainHandStack != prevMainHandStack) {
             world.playSoundFromEntity(null, this, Sounds.WEAPON_CHANGE, SoundCategory.NEUTRAL, 1, 1);
-            prevMainHandStack = currentMainHandStack;
         }
+        prevMainHandStack = currentMainHandStack;
         return super.getMainHandStack();
     }
 

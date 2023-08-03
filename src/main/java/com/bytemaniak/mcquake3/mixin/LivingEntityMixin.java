c3077df34c7c72479fb198dbf647f7127a5db8d5
bytemaniak <bytemaniak98@gmail.com>
2023-08-03 15:26:53 +0100

    Improve way in which invulnerability frames are ignored.
    Vanilla has a way to do this by adding the damage types to
    data/minecraft/tags/damage_type/*.json.

--- src/main/java/com/bytemaniak/mcquake3/mixin/LivingEntityMixin.java
+++ src/main/java/com/bytemaniak/mcquake3/mixin/LivingEntityMixin.java
@@ -6,9 +6,6 @@ import net.minecraft.entity.Entity;
 import net.minecraft.entity.EntityType;
 import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.damage.DamageSource;
-import net.minecraft.entity.damage.DamageType;
-import net.minecraft.registry.tag.DamageTypeTags;
-import net.minecraft.registry.tag.TagKey;
 import net.minecraft.world.World;
 import org.objectweb.asm.Opcodes;
 import org.spongepowered.asm.mixin.Mixin;
@@ -18,18 +15,6 @@ import org.spongepowered.asm.mixin.injection.At;
 public abstract class LivingEntityMixin extends Entity {
     public LivingEntityMixin(EntityType<?> type, World world) {
         super(type, world);
-    }
-
-    @WrapOperation(method = "damage", at = @At(value = "INVOKE", target = "Lnet/minecraft/entity/damage/DamageSource;isIn(Lnet/minecraft/registry/tag/TagKey;)Z"))
-    private boolean ignoreInvulnerabilityFrames(DamageSource damageSource, TagKey<DamageType> damageType, Operation<Boolean> original) {
-        // The code in LivingEntity has multiple isIn() checks in the damage() function
-        // The !BYPASSES_COOLDOWN check is used to override the invulnerability frames
-        // The !IS_EXPLOSION check is used to apply knockback to the entity when hurt
-        if (damageSource.getName().contains("mcquake3") &&
-                (damageType == DamageTypeTags.BYPASSES_COOLDOWN ||
-                 damageType == DamageTypeTags.IS_EXPLOSION)) {
-            return true;
-        } else return original.call(damageSource, damageType);
     }
 
     @WrapOperation(method = "onDamaged", at = @At(value = "FIELD", target = "Lnet/minecraft/entity/LivingEntity;hurtTime:I", opcode = Opcodes.PUTFIELD))

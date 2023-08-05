b67a6ec652ebdafd87cf031bd603622fff956007
bytemaniak <bytemaniak98@gmail.com>
2023-08-05 20:39:15 +0100

    Add health icon to Quake HUD

--- src/main/java/com/bytemaniak/mcquake3/gui/MCQuake3GuiRenderer.java
+++ src/main/java/com/bytemaniak/mcquake3/gui/MCQuake3GuiRenderer.java
@@ -36,13 +36,14 @@ public class MCQuake3GuiRenderer implements HudRenderCallback {
             int healthColor = (playerHealth < 100) ?
                     (((int)(0xFF * (100-playerHealth)/100.f)  << 16) + ((int)(0xFF * playerHealth/100.f) << 8)) :
                     (((int)(0xFF * (200-playerHealth)/100.f)) << 8)  + ((int)(0xFF * (playerHealth-100)/100.f));
+            drawText(matrixStack, "\uFFF0", x - 150, y - 16, 0x00FFFFFF);
             drawText(matrixStack, String.valueOf(playerHealth), x - 130, y, healthColor);
 
             QuakePlayer.WeaponSlot weapon = player.getCurrentWeapon();
             if (weapon != QuakePlayer.WeaponSlot.NONE && weapon.slot() > 0) {
                 int slotChar = '\uFFF0'+player.getCurrentWeapon().slot();
-                drawText(matrixStack, Character.toString((char)slotChar), x - 190, y - 16, 0x00FFFFFF);
-                drawText(matrixStack, String.valueOf(player.getCurrentAmmo()), x - 170, y, 0x00FFFFFF);
+                drawText(matrixStack, Character.toString((char)slotChar), x - 200, y - 16, 0x00FFFFFF);
+                drawText(matrixStack, String.valueOf(player.getCurrentAmmo()), x - 180, y, 0x00FFFFFF);
             }
         }
     }

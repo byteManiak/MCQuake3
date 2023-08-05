9086a5f2835e681f081d83b94eeba83eb851c140
bytemaniak <bytemaniak98@gmail.com>
2023-08-05 12:26:41 +0100

    Show damage effect when attacking mobs

--- src/main/java/com/bytemaniak/mcquake3/mixin/LivingEntityMixin.java
+++ src/main/java/com/bytemaniak/mcquake3/mixin/LivingEntityMixin.java
@@ -6,6 +6,7 @@ import net.minecraft.entity.Entity;
 import net.minecraft.entity.EntityType;
 import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.damage.DamageSource;
+import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.world.World;
 import org.objectweb.asm.Opcodes;
 import org.spongepowered.asm.mixin.Mixin;
@@ -20,7 +21,7 @@ public abstract class LivingEntityMixin extends Entity {
     @WrapOperation(method = "onDamaged", at = @At(value = "FIELD", target = "Lnet/minecraft/entity/LivingEntity;hurtTime:I", opcode = Opcodes.PUTFIELD))
     private void cancelHurtTilt(LivingEntity entity, int value, Operation<Void> original) {
         DamageSource lastDamageSource = entity.getRecentDamageSource();
-        if (lastDamageSource != null && lastDamageSource.getName().contains("mcquake3"))
+        if (lastDamageSource != null && entity instanceof PlayerEntity && lastDamageSource.getName().contains("mcquake3"))
             entity.hurtTime = 0;
         else original.call(entity, value);
     }

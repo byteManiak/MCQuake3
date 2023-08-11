1d7ebf1c9477d8cc097281e482947457d4a15b12
bytemaniak <bytemaniak98@gmail.com>
2023-08-12 00:30:49 +0100

    Skip scrolling empty hotbar slots.
    This will eventually be disabled by default and a toggle added for it.

---
+++ src/main/java/com/bytemaniak/mcquake3/mixin/MouseMixin.java
@@ -0,0 +1,37 @@
+package com.bytemaniak.mcquake3.mixin;
+
+import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
+import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
+import net.minecraft.client.Mouse;
+import net.minecraft.entity.player.PlayerInventory;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+
+@Mixin(Mouse.class)
+public class MouseMixin {
+    @WrapOperation(method = "onMouseScroll", at = @At(value = "INVOKE", target = "Lnet/minecraft/entity/player/PlayerInventory;scrollInHotbar(D)V"))
+    private void skipEmptySlotInHotbar(PlayerInventory inventory, double scrollAmount, Operation<Integer> original) {
+        boolean hotbarEmpty = true;
+        for (int i = 0; i < 9; i++)
+            if (!inventory.main.get(i).isEmpty()) {
+                hotbarEmpty = false;
+                break;
+            }
+
+        if (hotbarEmpty) original.call(inventory, scrollAmount);
+        else {
+            int i = (int) Math.signum(scrollAmount);
+            int slot = inventory.selectedSlot - i;
+
+            if (slot < 0) slot += 9;
+            if (slot >= 9) slot -= 9;
+
+            while (inventory.main.get(slot).isEmpty()) {
+                slot -= i;
+                if (slot < 0) slot += 9;
+                if (slot >= 9) slot -= 9;
+            }
+            inventory.selectedSlot = slot;
+        }
+    }
+}

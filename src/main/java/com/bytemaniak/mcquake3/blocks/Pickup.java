b7c964778032148e0c55838829273fdf518a3873
bytemaniak <bytemaniak98@gmail.com>
2023-08-18 13:16:57 +0100

    Now pickups are breakable only with the tool.
    Add loot tables for all pickups.

--- src/main/java/com/bytemaniak/mcquake3/blocks/Pickup.java
+++ src/main/java/com/bytemaniak/mcquake3/blocks/Pickup.java
@@ -1,10 +1,15 @@
 package com.bytemaniak.mcquake3.blocks;
 
+import com.bytemaniak.mcquake3.registry.Items;
 import net.fabricmc.fabric.api.object.builder.v1.block.FabricBlockSettings;
 import net.minecraft.block.*;
 import net.minecraft.block.entity.BlockEntity;
 import net.minecraft.block.entity.BlockEntityTicker;
 import net.minecraft.block.entity.BlockEntityType;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.util.ActionResult;
+import net.minecraft.util.Hand;
+import net.minecraft.util.hit.BlockHitResult;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.shape.VoxelShape;
 import net.minecraft.world.BlockView;
@@ -15,7 +20,7 @@ public abstract class Pickup extends BlockWithEntity implements BlockEntityProvi
     private static final VoxelShape SHAPE = Block.createCuboidShape(3, 0, 3, 13, 10, 13);
 
     public Pickup() {
-        super(FabricBlockSettings.of(Material.AIR).noCollision().requiresTool());
+        super(FabricBlockSettings.of(Material.STONE).strength(-1, 360000).noCollision().requiresTool());
     }
 
     @Override
@@ -27,5 +32,16 @@ public abstract class Pickup extends BlockWithEntity implements BlockEntityProvi
     @Override
     public <T extends BlockEntity> BlockEntityTicker<T> getTicker(World world, BlockState state, BlockEntityType<T> type) {
         return PickupEntity::tick;
+    }
+
+    @Override
+    public ActionResult onUse(BlockState state, World world, BlockPos pos, PlayerEntity player, Hand hand, BlockHitResult hit) {
+        if (!world.isClient) {
+            if (player.getStackInHand(hand).isOf(Items.TOOL)) {
+                world.breakBlock(pos, true);
+                return ActionResult.SUCCESS;
+            }
+        }
+        return super.onUse(state, world, pos, player, hand, hit);
     }
 }

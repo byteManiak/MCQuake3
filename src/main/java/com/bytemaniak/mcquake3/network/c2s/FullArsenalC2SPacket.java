bb8ad6859fbc902dabf15b93602a81cc7f4aaa6b
bytemaniak <bytemaniak98@gmail.com>
2024-01-24 17:29:39 +0000

    Restrict full arsenal to server operators

--- src/main/java/com/bytemaniak/mcquake3/network/c2s/FullArsenalC2SPacket.java
+++ src/main/java/com/bytemaniak/mcquake3/network/c2s/FullArsenalC2SPacket.java
@@ -7,17 +7,22 @@ import net.minecraft.network.PacketByteBuf;
 import net.minecraft.server.MinecraftServer;
 import net.minecraft.server.network.ServerPlayNetworkHandler;
 import net.minecraft.server.network.ServerPlayerEntity;
+import net.minecraft.text.Text;
 
 public class FullArsenalC2SPacket {
     public static void receive(MinecraftServer server, ServerPlayerEntity player, ServerPlayNetworkHandler handler, PacketByteBuf buf, PacketSender sender) {
-        player.giveItemStack(new ItemStack(Items.GAUNTLET));
-        player.giveItemStack(new ItemStack(Items.MACHINEGUN));
-        player.giveItemStack(new ItemStack(Items.SHOTGUN));
-        player.giveItemStack(new ItemStack(Items.GRENADE_LAUNCHER));
-        player.giveItemStack(new ItemStack(Items.ROCKET_LAUNCHER));
-        player.giveItemStack(new ItemStack(Items.LIGHTNING_GUN));
-        player.giveItemStack(new ItemStack(Items.RAILGUN));
-        player.giveItemStack(new ItemStack(Items.PLASMAGUN));
-        player.giveItemStack(new ItemStack(Items.BFG10K));
+        if (player.hasPermissionLevel(2)) {
+            player.giveItemStack(new ItemStack(Items.GAUNTLET));
+            player.giveItemStack(new ItemStack(Items.MACHINEGUN));
+            player.giveItemStack(new ItemStack(Items.SHOTGUN));
+            player.giveItemStack(new ItemStack(Items.GRENADE_LAUNCHER));
+            player.giveItemStack(new ItemStack(Items.ROCKET_LAUNCHER));
+            player.giveItemStack(new ItemStack(Items.LIGHTNING_GUN));
+            player.giveItemStack(new ItemStack(Items.RAILGUN));
+            player.giveItemStack(new ItemStack(Items.PLASMAGUN));
+            player.giveItemStack(new ItemStack(Items.BFG10K));
+        } else {
+            player.sendMessageToClient(Text.of("MCQuake3: You do not have access to that command."), false);
+        }
     }
 }

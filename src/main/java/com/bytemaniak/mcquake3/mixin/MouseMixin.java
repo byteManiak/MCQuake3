83c4b6bf2e64f994843b8213f9fa5b27fad67f44
bytemaniak <bytemaniak98@gmail.com>
2024-01-27 22:13:28 +0000

    Only disable empty hotbar scrolling when Quake GUI is enabled.

--- src/main/java/com/bytemaniak/mcquake3/mixin/MouseMixin.java
+++ src/main/java/com/bytemaniak/mcquake3/mixin/MouseMixin.java
@@ -1,7 +1,9 @@
 package com.bytemaniak.mcquake3.mixin;
 
+import com.bytemaniak.mcquake3.entity.QuakePlayer;
 import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
 import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
+import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.Mouse;
 import net.minecraft.entity.player.PlayerInventory;
 import org.spongepowered.asm.mixin.Mixin;
@@ -12,13 +14,14 @@ public class MouseMixin {
     @WrapOperation(method = "onMouseScroll", at = @At(value = "INVOKE", target = "Lnet/minecraft/entity/player/PlayerInventory;scrollInHotbar(D)V"))
     private void skipEmptySlotInHotbar(PlayerInventory inventory, double scrollAmount, Operation<Integer> original) {
         boolean hotbarEmpty = true;
+        QuakePlayer qPlayer = (QuakePlayer) MinecraftClient.getInstance().player;
         for (int i = 0; i < 9; i++)
             if (!inventory.main.get(i).isEmpty()) {
                 hotbarEmpty = false;
                 break;
             }
 
-        if (hotbarEmpty) original.call(inventory, scrollAmount);
+        if (hotbarEmpty || !qPlayer.quakeGuiEnabled()) original.call(inventory, scrollAmount);
         else {
             int i = (int) Math.signum(scrollAmount);
             int slot = inventory.selectedSlot - i;

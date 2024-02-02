df5c48d8a527a28598058d70b91fc578b39f1042
bytemaniak <bytemaniak98@gmail.com>
2024-02-02 16:57:54 +0000

    When giving ammo to player, try to place it in the non-hotbar inventory first

--- src/main/java/com/bytemaniak/mcquake3/blocks/weapon/WeaponPickup.java
+++ src/main/java/com/bytemaniak/mcquake3/blocks/weapon/WeaponPickup.java
@@ -6,7 +6,9 @@ import com.bytemaniak.mcquake3.items.Weapon;
 import net.minecraft.block.BlockState;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.entity.player.PlayerInventory;
 import net.minecraft.item.ItemStack;
+import net.minecraft.util.collection.DefaultedList;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
 
@@ -23,7 +25,25 @@ public abstract class WeaponPickup extends Pickup {
                 if (!player.getInventory().containsAny(t -> t.isOf(weapon)))
                     player.giveItemStack(new ItemStack(weapon));
                 // TODO: Limit ammo usage once Quake server mode is implemented
-                player.giveItemStack(new ItemStack(weapon.ammoType, weapon.ammoBoxCount));
+                PlayerInventory inventory = player.getInventory();
+                DefaultedList<ItemStack> main = inventory.main;
+                ItemStack ammo = new ItemStack(weapon.ammoType, weapon.ammoBoxCount);
+
+                // Prioritize the non-hotbar inventory for ammo pickup
+                for (int i = 9; i < main.size(); ++i) {
+                    if (ammo.getCount() == 0) break;
+
+                    if (main.get(i).isOf(weapon.ammoType)) {
+                        ammo.setCount(inventory.addStack(i, ammo));
+                    }
+                    else if (main.get(i).isEmpty()) {
+                        player.getInventory().insertStack(i, ammo);
+                        break;
+                    }
+                }
+
+                player.getInventory().insertStack(ammo);
+
                 world.markDirty(pos);
             }
         }

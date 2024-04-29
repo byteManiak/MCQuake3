8f7f3507267ebe64f014242c5a17ed88348f6054
bytemaniak <bytemaniak98@gmail.com>
2024-04-29 13:16:21 +0100

    Add scoreboard objectives for medals

--- src/main/java/com/bytemaniak/mcquake3/registry/Statistics.java
+++ src/main/java/com/bytemaniak/mcquake3/registry/Statistics.java
@@ -2,16 +2,20 @@ package com.bytemaniak.mcquake3.registry;
 
 import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
+import net.minecraft.scoreboard.ScoreboardCriterion;
 import net.minecraft.stat.Stats;
 import net.minecraft.util.Identifier;
 
 public class Statistics {
     private static final String EXCELLENT = "excellent_medals";
     public static final Identifier EXCELLENT_MEDALS = new Identifier("mcquake3:"+EXCELLENT);
+    public static final ScoreboardCriterion EXCELLENT_CRITERIA = ScoreboardCriterion.create("mcquake3."+EXCELLENT);
     private static final String IMPRESSIVE = "impressive_medals";
     public static final Identifier IMPRESSIVE_MEDALS = new Identifier("mcquake3:"+IMPRESSIVE);
+    public static final ScoreboardCriterion IMPRESSIVE_CRITERIA = ScoreboardCriterion.create("mcquake3."+IMPRESSIVE);
     private static final String GAUNTLET = "gauntlet_medals";
     public static final Identifier GAUNTLET_MEDALS = new Identifier("mcquake3:"+GAUNTLET);
+    public static final ScoreboardCriterion GAUNTLET_CRITERIA = ScoreboardCriterion.create("mcquake3."+GAUNTLET);
 
     private static void registerStat(String s, Identifier id) {
         Registry.register(Registries.CUSTOM_STAT, s, id);

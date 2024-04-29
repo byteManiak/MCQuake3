c72fd0d6267e8e76728459526b08c4e926ee4a9a
bytemaniak <bytemaniak98@gmail.com>
2024-04-29 17:59:15 +0100

    1.20.4 build fix

--- src/main/java/com/bytemaniak/mcquake3/network/c2s/AddMedalC2SPacket.java
+++ src/main/java/com/bytemaniak/mcquake3/network/c2s/AddMedalC2SPacket.java
@@ -3,7 +3,7 @@ package com.bytemaniak.mcquake3.network.c2s;
 import com.bytemaniak.mcquake3.registry.Statistics;
 import net.fabricmc.fabric.api.networking.v1.PacketSender;
 import net.minecraft.network.PacketByteBuf;
-import net.minecraft.scoreboard.ScoreboardPlayerScore;
+import net.minecraft.scoreboard.ScoreAccess;
 import net.minecraft.server.MinecraftServer;
 import net.minecraft.server.network.ServerPlayNetworkHandler;
 import net.minecraft.server.network.ServerPlayerEntity;
@@ -13,21 +13,20 @@ import java.util.function.Consumer;
 public class AddMedalC2SPacket {
     public static void receive(MinecraftServer server, ServerPlayerEntity player, ServerPlayNetworkHandler handler, PacketByteBuf buf, PacketSender sender) {
         byte value = buf.readByte();
-        String name = player.getEntityName();
-        Consumer<ScoreboardPlayerScore> incrementScore = ScoreboardPlayerScore::incrementScore;
+        Consumer<ScoreAccess> incrementScore = ScoreAccess::incrementScore;
 
         switch (value) {
             case 1 -> {
                 player.incrementStat(Statistics.EXCELLENT_MEDALS);
-                player.getScoreboard().forEachScore(Statistics.EXCELLENT_CRITERIA, name, incrementScore);
+                player.getScoreboard().forEachScore(Statistics.EXCELLENT_CRITERIA, player, incrementScore);
             }
             case 2 -> {
                 player.incrementStat(Statistics.IMPRESSIVE_MEDALS);
-                player.getScoreboard().forEachScore(Statistics.IMPRESSIVE_CRITERIA, name, incrementScore);
+                player.getScoreboard().forEachScore(Statistics.IMPRESSIVE_CRITERIA, player, incrementScore);
             }
             case 3 -> {
                 player.incrementStat(Statistics.GAUNTLET_MEDALS);
-                player.getScoreboard().forEachScore(Statistics.GAUNTLET_CRITERIA, name, incrementScore);
+                player.getScoreboard().forEachScore(Statistics.GAUNTLET_CRITERIA, player, incrementScore);
             }
         }
     }

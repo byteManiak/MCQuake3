0e83f7204db994d619f74ee7af26ba57f94f51a9
bytemaniak <bytemaniak98@gmail.com>
2024-08-14 00:23:01 +0100

    Hide player names in Quake GUI mode

---
+++ src/main/java/com/bytemaniak/mcquake3/mixin/misc/EntityRendererMixin.java
@@ -0,0 +1,16 @@
+package com.bytemaniak.mcquake3.mixin.misc;
+
+import com.bytemaniak.mcquake3.util.QuakePlayer;
+import net.minecraft.client.MinecraftClient;
+import net.minecraft.client.render.entity.EntityRenderer;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.ModifyVariable;
+
+@Mixin(EntityRenderer.class)
+public class EntityRendererMixin {
+    @ModifyVariable(method = "renderLabelIfPresent", at = @At(value = "STORE", ordinal = 0))
+    private boolean quakeGUIHidePlayerNames(boolean hide) {
+        return !((QuakePlayer)MinecraftClient.getInstance().player).quakeGuiEnabled() && hide;
+    }
+}

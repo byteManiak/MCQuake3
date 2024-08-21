cf067e48c99f48a36fb6b871a352ff6529b61ae6
bytemaniak <bytemaniak98@gmail.com>
2024-08-21 11:50:22 +0100

    Put weapons in correct order on Quake maps

--- src/main/java/com/bytemaniak/mcquake3/blocks/weapon/WeaponPickup.java
+++ src/main/java/com/bytemaniak/mcquake3/blocks/weapon/WeaponPickup.java
@@ -4,6 +4,7 @@ import com.bytemaniak.mcquake3.blocks.Pickup;
 import com.bytemaniak.mcquake3.blocks.PickupEntity;
 import com.bytemaniak.mcquake3.items.Weapon;
 import com.bytemaniak.mcquake3.util.MiscUtils;
+import com.bytemaniak.mcquake3.util.QuakePlayer;
 import net.minecraft.block.BlockState;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.player.PlayerEntity;
@@ -21,8 +22,12 @@ public abstract class WeaponPickup extends Pickup {
         if (!world.isClient) {
             PickupEntity weaponPickup = (PickupEntity) world.getBlockEntity(pos);
             if (entity instanceof PlayerEntity player && weaponPickup.use()) {
-                if (!player.getInventory().containsAny(t -> t.isOf(weapon)))
-                    player.giveItemStack(new ItemStack(weapon));
+                if (!player.getInventory().containsAny(t -> t.isOf(weapon))) {
+                    if (((QuakePlayer) player).playingQuakeMap())
+                        player.getInventory().insertStack(weapon.slot, new ItemStack(weapon));
+                    else
+                        player.giveItemStack(new ItemStack(weapon));
+                }
 
                 if (!player.isCreative()) {
                     // TODO: Limit ammo usage once Quake server mode is implemented

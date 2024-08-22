6991798809e75760d09e8f2d40590c692b681ca7
bytemaniak <bytemaniak98@gmail.com>
2024-08-22 18:39:03 +0100

    Fix crash on server

---
+++ src/main/java/com/bytemaniak/mcquake3/registry/ClientEvents.java
@@ -0,0 +1,15 @@
+package com.bytemaniak.mcquake3.registry;
+
+import com.bytemaniak.mcquake3.network.events.MessageDing;
+import net.fabricmc.api.EnvType;
+import net.fabricmc.api.Environment;
+import net.fabricmc.fabric.api.client.message.v1.ClientReceiveMessageEvents;
+
+@Environment(EnvType.CLIENT)
+public class ClientEvents {
+    public static final MessageDing MESSAGE_DING = new MessageDing();
+
+    public static void registerEvents() {
+        ClientReceiveMessageEvents.CHAT.register(MESSAGE_DING);
+    }
+}

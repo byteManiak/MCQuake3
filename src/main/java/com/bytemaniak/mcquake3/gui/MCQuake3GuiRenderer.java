83fceaec8e4fc7348247841c973d56933fec6e6f
bytemaniak <bytemaniak98@gmail.com>
2024-09-16 17:08:24 +0100

    Only render energy shield HUD in Quake arena

--- src/main/java/com/bytemaniak/mcquake3/gui/MCQuake3GuiRenderer.java
+++ src/main/java/com/bytemaniak/mcquake3/gui/MCQuake3GuiRenderer.java
@@ -25,27 +25,27 @@ public class MCQuake3GuiRenderer implements HudRenderCallback {
 
         QuakePlayer player = (QuakePlayer) MinecraftClient.getInstance().player;
 
-        Window window = MinecraftClient.getInstance().getWindow();
-        int x = window.getScaledWidth()/2;
-        int y = window.getScaledHeight() - 10;
-
-        int id = player.getCurrentQuakeWeaponId();
-        if (id > 0) {
-            int slotChar = '\uFFF0'+player.getCurrentQuakeWeaponId();
-            MiscUtils.drawText(matrixStack, Character.toString((char)slotChar), x - 150, y - 16, 0x00FFFFFF);
-            if (plr.getMainHandStack().getItem() instanceof Weapon weapon) {
-                int weaponAmmo = 0;
-
-                for (int i = 0; i < plr.getInventory().size(); i++) {
-                    ItemStack currentStack = plr.getInventory().getStack(i);
-                    if (currentStack.isOf(weapon.ammoType))
-                        weaponAmmo += currentStack.getCount();
-                }
-                MiscUtils.drawText(matrixStack, String.valueOf(weaponAmmo), x - 130, y, 0x00FFFFFF);
-            }
-        }
-
         if (player.inQuakeArena()) {
+            Window window = MinecraftClient.getInstance().getWindow();
+            int x = window.getScaledWidth()/2;
+            int y = window.getScaledHeight() - 10;
+            int w = window.getScaledWidth() - 32;
+            int id = player.getCurrentQuakeWeaponId();
+            if (id > 0) {
+                int slotChar = '\uFFF0'+player.getCurrentQuakeWeaponId();
+                MiscUtils.drawText(matrixStack, Character.toString((char)slotChar), x - 150, y - 16, 0x00FFFFFF);
+                if (plr.getMainHandStack().getItem() instanceof Weapon weapon) {
+                    int weaponAmmo = 0;
+
+                    for (int i = 0; i < plr.getInventory().size(); i++) {
+                        ItemStack currentStack = plr.getInventory().getStack(i);
+                        if (currentStack.isOf(weapon.ammoType))
+                            weaponAmmo += currentStack.getCount();
+                    }
+                    MiscUtils.drawText(matrixStack, String.valueOf(weaponAmmo), x - 130, y, 0x00FFFFFF);
+                }
+            }
+
             int playerHealth = (int)(plr.getHealth()*5);
             int healthColor = (playerHealth < 100) ?
                     (((int)(0xFF * (100-playerHealth)/100.f)  << 16) + ((int)(0xFF * playerHealth/100.f) << 8)) :
@@ -62,8 +62,6 @@ public class MCQuake3GuiRenderer implements HudRenderCallback {
                     (((int)(0xFF * (200-playerArmor)/100.f)) << 8)  + ((int)(0xFF * (playerArmor-100)/100.f));
             MiscUtils.drawText(matrixStack, "\uFFF9", x + 105, y - 16, 0x00FFFFFF);
             MiscUtils.drawText(matrixStack, String.valueOf(playerArmor), x + 125, y, armorColor);
-
-            int w = window.getScaledWidth() - 32;
 
             MiscUtils.drawText(matrixStack, String.valueOf(QuakeMatchState.FRAG_LIMIT), w - 60, y - 28, 0x00FFFFFF);
 
